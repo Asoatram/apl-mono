82df148f528568d6d4806d4a1c46e05776850edb
Muhamad Daffa Azfa Rabbani <109969988+Asoatram@users.noreply.github.com>
2025-05-30 15:55:39 +0700

    feat: added recipe

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from controller import AuthController, RecipeController
+from core.db import Base, engine
+from contextlib import asynccontextmanager
 
-app = FastAPI()
 
-@app.get("/")
-async def read_root():
-    return {"message": "Hello, World!"}
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(AuthController.router, prefix="/auth", tags=["Authentication"])
+
+app.include_router(RecipeController.router, prefix="/api/v1", tags=["Recipes"], )
