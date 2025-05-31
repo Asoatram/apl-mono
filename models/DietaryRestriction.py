59ad0aedc477fea1ae30b13fa486185b898038e9
Kholil-Asjaduddin <kholilasjaduddin@mail.ugm.ac.id>
2025-05-31 23:32:04 +0700

    feat: add REST API for add new user dietary restriction

--- models/DietaryRestriction.py
+++ models/DietaryRestriction.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from models import Base
 
 class DietaryRestriction(Base):
@@ -6,3 +6,9 @@ class DietaryRestriction(Base):
     dietaryrestrictionid = Column(Integer, primary_key=True, index=True, autoincrement=True)
     name = Column(String, unique=True, nullable=False)
     description = Column(String, nullable=True)
+    
+class UserDietaryRestriction(Base):
+    __tablename__ = "userdietaryrestriction"
+    userdietaryrestrictionid = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    userid = Column(Integer, ForeignKey("users.userid"), nullable=False)
+    dietaryrestrictionid = Column(Integer, ForeignKey("dietaryrestriction.dietaryrestrictionid"), nullable=False)
